f880f1faebe729dfac1f6ffd90e6ef91b2155211
Ezeana Micheal <michael.c.ezeana@gmail.com>
2025-11-18 11:50:25 +0100

    settings api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
-// export const API_BASE_URL = 'http://16.16.182.74/api/v1';
+export const API_BASE_URL = 'http://16.16.182.74/api/v1';
 
-export const API_BASE_URL = 'http://127.0.0.1:5000/api/v1';
+// export const API_BASE_URL = 'http://127.0.0.1:5000/api/v1';
 
 
 // Types for API responses
